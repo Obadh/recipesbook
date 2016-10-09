d757a80cbda8de74a18471ca935de1fc7f6c3b0f
ahmed alassafy <ahmedalassafy1989@gmail.com>
2016-10-09 13:52:32 +0100

    modified main

--- server/app.js
+++ server/app.js
@@ -22,6 +22,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use("/recipesbook", express.static(path.join(__dirname, '../Main')));
+
 
 app.use('/', routes);
 app.use('/users', users);
