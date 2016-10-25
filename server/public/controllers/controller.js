473c5fc6a62a196f85eb2a9918a004f319592229
Obadh Koujan <obadhk@live.co.uk>
2016-10-25 21:43:13 +0200

    move to angular

---
+++ server/public/controllers/controller.js
@@ -0,0 +1,20 @@
+angular.
+    module('recepie', []).
+    controller('search-recepie',function($scope, $http){
+      console.log($scope);
+      $http({
+  method: 'POST',
+  url: '/get_recipe',
+  data: {
+    food: [ 'Broccoli', 'Parsley']
+  }
+}).then(function successCallback(response) {
+    // this callback will be called asynchronously
+    // when the response is available
+    console.log('Recepies', response)
+  }, function errorCallback(response) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+    console.log('Mauro is stupid');
+  });
+    })
--- server/public/javascripts/js-index.js
+++ server/public/javascripts/js-index.js
@@ -3,24 +3,3 @@ $(document).ready(function(e){
 				$(this).toggleClass("check");
 			});
 	});
-
-  angular.
-    module('recepie', []).
-    controller('search-recepie',function($scope, $http){
-      console.log($scope);
-      $http({
-  method: 'POST',
-  url: '/get_recipe',
-  data: {
-    food: req.body.name
-  }
-}).then(function successCallback(response) {
-    // this callback will be called asynchronously
-    // when the response is available
-    console.log('Recepies', response)
-  }, function errorCallback(response) {
-    // called asynchronously if an error occurs
-    // or server returns response with an error status.
-    console.log('Mauro is stupid');
-  });
-    })
